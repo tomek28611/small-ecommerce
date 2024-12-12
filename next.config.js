6b58b8e4cc03a853c300b9e72318d3c0f18262e7
Tom <tomek286@yahoo.com>
2024-12-12 11:38:24 +0100

    All-Ptoducts

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    API_URL: "http://localhost:3000",
+   
+  },
+};
+
+module.exports = nextConfig;
